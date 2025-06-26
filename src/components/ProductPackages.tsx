fdb213d36fd380d9a96045b303ab742ef0847e61
muragewanjohi <muragewanjohi@gmail.com>
2025-06-26 22:23:23 +0300

    Comment out popular package indicator in ProductPackages component for future implementation

--- src/components/ProductPackages.tsx
+++ src/components/ProductPackages.tsx
@@ -67,13 +67,13 @@ const ProductPackages = () => {
                 pkg.popular ? 'ring-4 ring-orange-500' : ''
               }`}
             >
-              {pkg.popular && (
+              {/* {pkg.popular && (
                 <div className="absolute -top-4 left-1/2 transform -translate-x-1/2">
                   <span className="bg-orange-500 text-white px-4 py-2 rounded-full text-sm font-semibold">
                     Most Popular
                   </span>
                 </div>
-              )}
+              )} */}
 
               <div className={`bg-gradient-to-r ${pkg.color} text-white p-8 rounded-t-2xl`}>
                 <div className="text-4xl mb-4">{pkg.icon}</div>
